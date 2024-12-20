b87262d82ed7ea36dbc50b5ba83ef2f46b3ac3a0
LEZORAY Nicolas <70590408+nicolaslezoray@users.noreply.github.com>
2024-12-20 09:20:42 +0100

    Create jenkins.php

---
+++ jenkins.php
@@ -0,0 +1,13 @@
+<?php
+/* Requires the Docker Pipeline plugin */
+pipeline {
+    agent { docker { image 'php:8.4.1-alpine3.21' } }
+    stages {
+        stage('build') {
+            steps {
+                sh 'php --version'
+            }
+        }
+    }
+}
+?>
